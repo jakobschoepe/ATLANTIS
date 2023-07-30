dcc1aa62dc446592308785099531d09441102848
Jakob Schöpe <jakobschoepe@protonmail.com>
2023-07-31 01:11:22 +0200

    Create app.R

---
+++ app.R
@@ -0,0 +1,91 @@
+library(DT)
+library(RSQLite)
+library(shiny)
+library(shinyjs)
+library(shinythemes)
+
+version <- 'Version: 0.1b (31 July 2023)'
+creator <- 'Jakob Schöpe'
+year <- format(Sys.Date(), '%Y')
+colnamesTasks <- c('Unique Identifier',
+              'Title',
+              'Program Name',
+              'Output Type',
+              'Output Name',
+              'Reference',
+              'Programming Status',
+              'Statistical Programmer',
+              'Programming Specifications',
+              'Programming Completion Date",
+              'Quality Controlling Status',
+              'Quality Controller',
+              'Quality Controlling Specifications',
+              'Quality Controlling Completion Date',
+              'Delivery Status',
+              'Delivery Date'
+              )
+
+colnamesIssues <- c()
+
+ui <- fluidPage(tags$head(HTML("<title>ATLANTIS</title> <link rel='icon' type='image/gif/png' href='favicon.png'>")),
+                shinyjs::useShinyjs(),
+                navbarPage(strong('ATLANTIS'),
+                           navbarMenu('Home',
+                                      tabPanel('Projects',
+                                               div(div(style='display: inline-block; width: 135px;', actionButton(inputId = 'createProject00', label = 'Create project', icon = icon('plus', lib = 'font-awesome'))),
+                                                   div(style='display: inline-block; width: 100px;', actionButton(inputId = 'editProject00', label = 'Edit project', icon = icon('pen-to-square', lib = 'font-awesome')))
+                                                  ),
+                                               br(),
+                                               br(),
+                                               p(HTML('&#169;'), year, creator, align = 'center')
+                                              ),
+                                      tabPanel('Tasks', 
+                                               selectInput(inputId = 'tasksSelectProject', label = 'Project', choices = queryTasksSelectProject()),
+                                               br(),
+                                               div(div(style='display: inline-block; width: 100px;', actionButton(inputId = 'createTask00', label = 'Create task', icon = icon('plus', lib = 'font-awesome'))),
+                                                   div(style='display: inline-block; width: 100px;', actionButton(inputId = 'editTask00', label = 'Edit task', icon = icon('pen-to-square', lib = 'font-awesome')))
+                                                  ),
+                                               br(),
+                                               br(),
+                                               DTOutput('tasks'),
+                                               br(),
+                                               br(),
+                                               p(HTML('&#169;'), year, creator, align = 'center')
+                                              ),
+                                      tabPanel('Issues',
+                                               selectInput(inputId = 'issuesSelectProject', label = "Project", choices = queryIssuesSelectProject()),
+                                               br(),
+                                               div(div(style='display: inline-block; width: 120px;', actionButton(inputId = 'createIssue00', label = 'Create issue', icon = icon('plus', lib = 'font-awesome'))),
+                                                   div(style='display: inline-block; width: 120px;', actionButton(inputId = 'editIssue00', label = 'Edit issue', icon = icon('pen-to-square', lib = 'font-awesome'))),
+                                                   div(style='display: inline-block; width: 120px;', actionButton(inputId = 'replyIssue00', label = 'Reply to issue', icon = icon('reply', lib = 'font-awesome')))
+                                                  ),
+                                               br(),
+                                               br(),
+                                               DTOutput('issues'),
+                                               br(),
+                                               br(),
+                                               p(HTML('&#169;'), year, creator, align = 'center')
+                                              )
+                                     ),
+                           navbarMenu('Help',
+                                      tabPanel('About', 
+                                               h4(strong('ATLANTIS'), align = 'center'),
+                                               h5(version, align = 'center'),
+                                               p("ATLANTIS is an open-source statistical programming tracking system.", align = "center"),
+                                               br(),
+                                               h4(strong("Software License Agreement"), align = "center"),
+                                               br(),
+                                               br(),
+                                               p(HTML('&#169;'), year, creator, align = 'center')
+                                              )
+                                     )
+))
+
+server <- function(input, output, session) {
+  source('./source/initialSetup.R', local = TRUE)$value
+  source('./source/createProject.R', local = TRUE)$value
+  source('./source/createIssue.R', local = TRUE)$value
+  source('./source/replyIssue.R', local = TURE)$value
+}
+
+shinyApp(ui, server)
