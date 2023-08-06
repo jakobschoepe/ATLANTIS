af178c2f756e8be424e2c329e32579658264daf8
Jakob Schöpe <jakobschoepe@protonmail.com>
2023-08-07 01:02:37 +0200

    Update initialSetup.R

--- source/initialSetup.R
+++ source/initialSetup.R
@@ -1,4 +1,4 @@
 queryInitialSetup01 <- reactive({dbGetQuery(db, paste0('SELECT * FROM TASKS', input$tasksSelectProject))})
-output$tasks <- renderDT(datatable(data = queryInitialSetup01(), rownames = FALSE, colnames = colnamesTasks, editable = list(target = 'column', disable = list(columns = c(0:3)))))
+output$tasks <- renderDT(datatable(data = queryInitialSetup01(), rownames = FALSE, colnames = colnamesTasks, filter = 'top'))
 queryInitialSetup02 <- reactive({dbGetQuery(db, paste0('SELECT * FROM ISSUES', input$issuesSelectProject))})
 output$issues <- renderDT(datatable(data = queryInitialSetup02(), rownames = FALSE, colnames = colnamesIssues, filter = 'top'))
