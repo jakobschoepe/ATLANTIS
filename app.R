62fb1e22634e67b274b9e63c0ecb3d23204e4969
Jakob Schöpe <jakobschoepe@protonmail.com>
2023-08-07 00:02:57 +0200

    Update app.R

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ version <- 'Version: 0.1b (6 August 2023)'
 creator <- 'Jakob Schöpe'
 year <- format(Sys.Date(), '%Y')
 colnamesTasks <- c('Unique Identifier', 'Title', 'Program Name', 'Output Type', 'Output Name', 'Reference', 'Programming Status', 'Statistical Programmer', 'Programming Specifications', 'Programming Completion Date', 'Quality Controlling Status', 'Quality Controller', 'Quality Controlling Specifications', 'Quality Controlling Completion Date', 'Delivery Status', 'Delivery Date')
-colnamesIssues <- c()
+colnamesIssues <- c('Unique Identifier', 'Issue Status', 'Issue Description', 'Issue Opening Date', 'Reply', 'Issue Closing Date')
 
 ui <- fluidPage(tags$head(HTML("<title>ATLANTIS</title> <link rel='icon' type='image/gif/png' href='favicon.png'>")),
                 shinyjs::useShinyjs(),
@@ -31,7 +31,8 @@ ui <- fluidPage(tags$head(HTML("<title>ATLANTIS</title> <link rel='icon' type='i
                                                br(),
                                                p(HTML('&#169;'), year, creator, align = 'center')
                                               ),
-                                      tabPanel('Tasks', 
+                                      tabPanel('Tasks',
+                                               textOutput(outputId = 'tasksNotification'),
                                                selectInput(inputId = 'tasksSelectProject', label = 'Project', choices = dbGetQuery(db, 'SELECT uniqueIdentifier FROM PROJECTS')),
                                                br(),
                                                div(div(style='display: inline-block; width: 100px;', actionButton(inputId = 'createTask00', label = 'Create task', icon = icon('plus', lib = 'font-awesome'))),
@@ -39,12 +40,13 @@ ui <- fluidPage(tags$head(HTML("<title>ATLANTIS</title> <link rel='icon' type='i
                                                   ),
                                                br(),
                                                br(),
-                                               DTOutput('tasks'),
+                                               DTOutput(outputId = 'tasks'),
                                                br(),
                                                br(),
                                                p(HTML('&#169;'), year, creator, align = 'center')
                                               ),
                                       tabPanel('Issues',
+                                               textOutput(outputId = 'issuesNotification'),
                                                selectInput(inputId = 'issuesSelectProject', label = "Project", choices = dbGetQuery(db, 'SELECT uniqueIdentifier FROM PROJECTS')),
                                                br(),
                                                div(div(style='display: inline-block; width: 120px;', actionButton(inputId = 'createIssue00', label = 'Create issue', icon = icon('plus', lib = 'font-awesome'))),
@@ -53,7 +55,7 @@ ui <- fluidPage(tags$head(HTML("<title>ATLANTIS</title> <link rel='icon' type='i
                                                   ),
                                                br(),
                                                br(),
-                                               DTOutput('issues'),
+                                               DTOutput(outputId = 'issues'),
                                                br(),
                                                br(),
                                                p(HTML('&#169;'), year, creator, align = 'center')
