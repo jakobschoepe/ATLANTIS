e1df7f9537b026409c55e3d1d3e5058e489e2216
Jakob Schöpe <jakobschoepe@protonmail.com>
2023-08-06 23:45:46 +0200

    Update createProject.R

--- source/createProject.R
+++ source/createProject.R
@@ -1,30 +1,14 @@
 observeEvent(input$createProject00, {
   showModal(modalDialog(title = 'Create new project',
                         textInput(inputId = 'createProject01', label = NULL, placeholder = 'Please enter a unique project identifier'),
-                        footer = tagList(modalButton('Cancel'), actionButton(inputId = 'createProject02', label = 'Submit'))))
+                        textInput(inputId = 'createProject02', label = NULL, placeholder = 'Please enter a project name'),
+                        footer = tagList(modalButton('Cancel'), actionButton(inputId = 'createProject03', label = 'Submit'))))
 })
 
-observeEvent(input$createProject02, {
-  queryCreateProject01 <- reactive({dbGetQuery(db, paste0('CREATE TABLE tbl', input$createProject01, ' (uniqueIdentifier varchar(255), 
-                                                                                                        title varchar(255), 
-                                                                                                        programName varchar(255), 
-                                                                                                        outputType varchar(255), 
-                                                                                                        outputName varchar(255), 
-                                                                                                        reference varchar(255), 
-                                                                                                        programmingStatus varchar(255), 
-                                                                                                        assignedStatisticalProgrammer varchar(255), 
-                                                                                                        programmingAssignmentDate varchar(255), 
-                                                                                                        programmingSpecifications varchar(255),
-                                                                                                        programmingDate varchar(255),
-                                                                                                        qualityControllingStatus varchar(255),
-                                                                                                        assignedQualityController varchar(255),
-                                                                                                        qualityControllingAssignmentDate varchar(255),
-                                                                                                        qualityControllingSpecifications varchar(255),
-                                                                                                        qualityControllingDate varchar(255),
-                                                                                                        deliveryStatus varchar(255),
-                                                                                                        deliveryDate varchar(255))'
-                                    ))
-                                   })
+observeEvent(input$createProject03, {
+  dbGetQuery(db, paste0('INSERT INTO PROJECTS (uniqueIdentifier, projectName) VALUES ("', input$createProject01, '", "', input$createProject02, '")')) 
+  queryCreateProject01 <- reactive({dbGetQuery(db, paste0('CREATE TABLE TASKS', input$createProject01, ' (uniqueIdentifier varchar(255), title varchar(255), programName varchar(255), outputType varchar(255), outputName varchar(255), reference varchar(255), programmingStatus varchar(255), assignedStatisticalProgrammer varchar(255), programmingAssignmentDate varchar(255), programmingSpecifications varchar(255), programmingDate varchar(255), qualityControllingStatus varchar(255), assignedQualityController varchar(255), qualityControllingAssignmentDate varchar(255), qualityControllingSpecifications varchar(255), qualityControllingDate varchar(255), deliveryStatus varchar(255), deliveryDate varchar(255))'))})
   queryCreateProject01()
-  
+  queryCreateProject02 <- reactive({dbGetQuery(db, paste0('CREATE TABLE ISSUES', input$createProject01, ' (uniqueIdentifier varchar(255), issueStatus varchar(255), issueDescription varchar(255), issueOpeningDate varchar(255), reply varchar(255), issueClosingDate varchar(255))'))})
+  queryCreateProject02()
 })
