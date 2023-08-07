9a0d3b90056db18c0f46d1ec41f25d7d048ee184
Jakob Schöpe <jakobschoepe@protonmail.com>
2023-08-07 17:21:37 +0200

    Update createProject.R

--- source/createProject.R
+++ source/createProject.R
@@ -2,10 +2,11 @@ observeEvent(input$createProject00, {
   showModal(modalDialog(title = 'Create new project',
                         textInput(inputId = 'createProject01', label = NULL, placeholder = 'Please enter a unique project identifier'),
                         textInput(inputId = 'createProject02', label = NULL, placeholder = 'Please enter a project name'),
-                        footer = tagList(modalButton('Cancel'), actionButton(inputId = 'createProject03', label = 'Submit'))))
+                        selectInput(inputId = 'createProject03', label = NULL, choices = c('Clinical trial', 'Observational study', 'Benefit Assessment')),
+                        footer = tagList(modalButton('Cancel'), actionButton(inputId = 'createProject05', label = 'Submit'))))
 })
 
-observeEvent(input$createProject03, {
+observeEvent(input$createProject05, {
   dbExecute(db, paste0('INSERT INTO PROJECTS (uniqueIdentifier, projectName) VALUES ("', input$createProject01, '", "', input$createProject02, '")')) 
   queryCreateProject01 <- reactive({dbExecute(db, paste0('CREATE TABLE TASKS', input$createProject01, ' (uniqueIdentifier varchar(255), title varchar(255), programName varchar(255), outputType varchar(255), outputName varchar(255), reference varchar(255), programmingStatus varchar(255), assignedStatisticalProgrammer varchar(255), programmingAssignmentDate varchar(255), programmingSpecifications varchar(255), programmingDate varchar(255), qualityControllingStatus varchar(255), assignedQualityController varchar(255), qualityControllingAssignmentDate varchar(255), qualityControllingSpecifications varchar(255), qualityControllingDate varchar(255), deliveryStatus varchar(255), deliveryDate varchar(255))'))})
   queryCreateProject01()
